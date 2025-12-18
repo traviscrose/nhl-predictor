a3a6023c297856ee00b94a9055f201a2b88a6d5c
traviscrose <traviscrose@gmail.com>
2025-12-18 09:56:34 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,26 @@
 from flask import Flask
 import os
+import psycopg2
 
 app = Flask(__name__)
 
+DATABASE_URL = os.environ.get("DATABASE_URL")
+
+def get_db_connection():
+    return psycopg2.connect(DATABASE_URL, sslmode="require")
+
 @app.route("/")
 def home():
-    return "NHL Predictor running on Render"
+    try:
+        conn = get_db_connection()
+        cur = conn.cursor()
+        cur.execute("SELECT 'PostgreSQL connected!'")
+        msg = cur.fetchone()[0]
+        cur.close()
+        conn.close()
+        return msg
+    except Exception as e:
+        return f"Database error: {e}"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
