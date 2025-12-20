adec0c73901fc48d38513ebd2a19b98b26591e97
traviscrose <traviscrose@gmail.com>
2025-12-19 23:25:17 -0600

    Update db.py

--- db.py
+++ db.py
@@ -1,12 +1,12 @@
-from sqlalchemy import create_engine
 import os
-import psycopg2
-from psycopg2.extras import RealDictCursor
+from sqlalchemy import create_engine
 
-def get_conn():
-    return psycopg2.connect(
-        os.environ["DATABASE_URL"],
-        cursor_factory=RealDictCursor
-    )
+DATABASE_URL = os.getenv("DATABASE_URL")
 
-engine = create_engine(DATABASE_URL, pool_pre_ping=True)
+if not DATABASE_URL:
+    raise RuntimeError("DATABASE_URL environment variable is not set")
+
+engine = create_engine(
+    DATABASE_URL,
+    pool_pre_ping=True
+)
