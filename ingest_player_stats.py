006a9bef44060002d9d716ff76e7f04bf5ba3dc6
traviscrose <traviscrose@gmail.com>
2025-12-20 18:04:25 -0600

    Update ingest_player_stats.py

--- ingest_player_stats.py
+++ ingest_player_stats.py
@@ -1,31 +1,5 @@
 from db import get_conn
 import requests
-from flask import Flask
-import os
-import psycopg2
-
-app = Flask(__name__)
-
-DATABASE_URL = os.environ.get("DATABASE_URL")
-
-def get_db_connection():
-    return psycopg2.connect(DATABASE_URL, sslmode="require")
-
-@app.route("/")
-def home():
-    try:
-        conn = get_db_connection()
-        cur = conn.cursor()
-        cur.execute("SELECT 'PostgreSQL connected!'")
-        msg = cur.fetchone()[0]
-        cur.close()
-        conn.close()
-        return msg
-    except Exception as e:
-        return f"Database error: {e}"
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
 
 BOXSCORE_URL = "https://api-web.nhle.com/v1/gamecenter/{game_id}/boxscore"
 
